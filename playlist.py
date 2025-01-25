9961cf8c09da85c0587de2fc777a9d3f4e86f238
Malu Vicentini <vicentinimalu1@gmail.com>
2025-01-25 13:57:04 -0300

    Update playlist.py

--- playlist.py
+++ playlist.py
@@ -1,72 +1,86 @@
-# Criando classe generica(Superclasse)
-class Programa: # Classe Mãe
-    
-    # Definindo quais serão os atributos (características) da classe mãe
+# SUPERCLASSE
+class Programa:
     def __init__(self, nome, ano):
-        self._nome = nome.title() 
-        self.ano = ano
-        self._likes = 0 # Atributo opcional: ao criar uma instância dessa classe, não é necessário atribuir valor a este atributo. 
+        self._nome = nome.title()  # Atributo protegido: Nome do programa formatado com a primeira letra maiúscula
+        self.ano = ano  # Ano de lançamento do programa
+        self._likes = 0  # Atributo protegido: Inicia com 0 likes
 
-    # Definindo o método propriedade: Obter likes
     @property
     def likes(self):
-        return self._likes # Retorna o valor atual do atributo "likes"
+        """Retorna o número de likes do programa."""
+        return self._likes
 
-    # Definindo método da instância: Modifica o valor do atributo "self._likes"
     def dar_like(self):
-        self._likes += 1 # Incrementa (adiciona) o valor do atributo "likes" em 1 sempre que é chamado.
+        """Incrementa o número de likes em 1."""
+        self._likes += 1
 
-    # Definindo o método propriedade: Obter nome
     @property
     def nome(self):
-        return self._nome # Retorna o valor atual do atributo "nome"
+        """Retorna o nome do programa."""
+        return self._nome
 
-    # Definindo o método setter: Define um novo valor para o atributo "self._nome"
     @nome.setter
     def nome(self, novo_nome):
-        self._nome = novo_nome.title() # O atributo "_nome" recebe um novo nome que será formatado com a primeira letra maiúscula
+        """Atualiza o nome do programa com a primeira letra maiúscula."""
+        self._nome = novo_nome.title()
 
     def __str__(self):
+        """Retorna uma representação em string do programa."""
         return f"{self._nome} - {self.ano} - {self._likes} Likes"
 
-# Criando classe filha (subclasse)
-class Filme(Programa): # A classe Programa é herdada, permitindo utilizarmos atributos e métodos dentro da classe filha
-    # Definindo quais são os atributos da classe filha:
+
+# SUBCLASSE
+class Filme(Programa):  # Herda os atributos e métodos da superclasse Programa
     def __init__(self, nome, ano, duracao):
-        # Herdando os atributos da classe mãe através do método "super()"
-        super().__init__(nome, ano) # Herdando nome e ano da classe mãe que já foram definidos
-        # Definindo atributo "duração"
-        self.duracao = duracao # Esse atributo não foi herdado, ele é exclusivo da classe FILME
+        super().__init__(nome, ano)  # Inicializa os atributos da superclasse
+        self.duracao = duracao  # Duração do filme em minutos
 
     def __str__(self):
+        """Retorna uma representação em string do filme, incluindo sua duração."""
         return f"{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes"
 
-# Criando classe filha (subclasse)
-class Serie(Programa): # A classe Programa é herdada, permitindo utilizarmos atributos e métodos dentro da classe filha
-    # Inicializando os atributos da classe filha
+
+# SUBCLASSE
+class Serie(Programa):  # Herda os atributos e métodos da superclasse Programa
     def __init__(self, nome, ano, temporadas):
-        # Herdando atributos da classe mãe
-        super().__init__(nome, ano) 
-        # Definindo o atributo "temporadas"
-        self.temporadas = temporadas # Esse atributo não foi herdado, ele é exclusivo da classe SERIE
+        super().__init__(nome, ano)  # Inicializa os atributos da superclasse
+        self.temporadas = temporadas  # Quantidade de temporadas da série
 
     def __str__(self):
+        """Retorna uma representação em string da série, incluindo suas temporadas."""
         return f"{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes"
 
-class Playlist(list):
-    def __init__(self, nome, programas):
-        self.nome = nome 
-        super().__init__(programas)
 
-# Criando uma instância a partir da classe "Filme"
-vingadores = Filme("Vingadores - guerra infinita", 2018, 160) # Argumentos que serão atribuídos aos atributos
-# Criando uma instância a partir da classe "Serie"
+# CLASSE COMPOSIÇÃO
+class Playlist:
+    def __init__(self, nome_playlist, programas):
+        """Inicializa a playlist com um nome e uma lista de programas (filmes ou séries)."""
+        self.nome_playlist = nome_playlist
+        self._programas = programas  # Lista que armazena os programas (objetos das subclasses Filme e Serie)
+
+    def __getitem__(self, item):
+        """Permite acessar os programas da playlist como uma lista normal."""
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        """Retorna todos os programas da playlist."""
+        return self._programas
+
+    @property
+    def tamanho(self):
+        """Retorna a quantidade de programas na playlist."""
+        return len(self._programas)
+
+
+# Instâncias(objetos) de filmes e séries
+vingadores = Filme("Vingadores - guerra infinita", 2018, 160)
 atlanta = Serie("atlanta", 2018, 2)
 tmep = Filme("Todo mundo em pânico", 1999, 100)
 demolidor = Serie("Demolidor", 2016, 2)
 
-# O objeto chama a função que permite dar likes
-vingadores.dar_like() 
+# Adicionando likes às instâncias
+vingadores.dar_like()
 tmep.dar_like()
 tmep.dar_like()
 tmep.dar_like()
@@ -74,14 +88,15 @@ tmep.dar_like()
 demolidor.dar_like()
 demolidor.dar_like()
 atlanta.dar_like()
-atlanta.dar_like() 
-atlanta.dar_like() 
+atlanta.dar_like()
+atlanta.dar_like()
 
+# Lista que recebe os objetos criados a partir das classes Filme e Serie
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+
+# Criando uma playlist com os filmes e séries
 playlist_fim_de_semana = Playlist("Fim de semana", filmes_e_series)
 
-print(f"Tamanho da playlist: {len(playlist_fim_de_semana)}")
-
-for programa in playlist_fim_de_semana: 
+# Itera pela playlist e imprime os detalhes de cada programa
+for programa in playlist_fim_de_semana.listagem:
     print(programa)
-
